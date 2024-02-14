03823570eaecf4c328415c8f1e24c172add9d7f2
Andrew McSparron <Amcsparron@albanyny.gov>
2024-02-14 09:07:57 -0500

    added PrePopulatedForm class, IntellitextBaseForm now subclasses this class

--- DjangoIntellitextFormField/IntellitextFormField.py
+++ DjangoIntellitextFormField/IntellitextFormField.py
@@ -5,10 +5,117 @@ from typing import Dict
 from django.db.models.query import QuerySet
 import logging
 
+from inspect import stack as istack
+from collections import ChainMap
+
+
 logger = logging.getLogger(__name__)
 
 
-class IntellitextBaseForm(Form):
+class PrePopulatedForm(Form):
+    @classmethod
+    def PrePopulateForm(cls, function_name: str = None, list_of_results: list = None):
+        """
+           Pre-populates a Django form for editing based on a given function name, row ID, and list of results.
+
+           Args:
+               function_name (str): The name of the form submission function.
+               row_id (int, optional): The ID of the row to be edited. Defaults to None.
+               list_of_results (list, optional): A list of dictionaries containing the query results. Defaults to None.
+               pre_populate (bool): Flag indicating whether to pre-populate the form. Defaults to False.
+
+           Returns:
+               django.forms.Form: An instance of the Django form pre-populated with values based on the provided parameters.
+
+           Raises:
+               AttributeError: Raised if 'initial_values' and 'row_id' are not provided when pre_populate is True.
+                               Raised if no row match is found, and the form cannot be pre-populated.
+
+           Notes:
+               - This function is designed to pre-populate a form for editing based on the provided parameters.
+               - It supports intelligent text fields with the assumption that they have 'Search' appended to their field names.
+               - The function_name parameter determines the form to be used.
+               - The row_id parameter specifies the ID of the row to be edited.
+               - The list_of_results parameter is a list of dictionaries containing the query results.
+               - If pre_populate is True, the function attempts to match the row_id with the 'id' field in the results.
+               - The function then remaps the results, replacing field names with their corresponding intelligent text fields.
+               - The pre-populated form is returned as an instance of the Django form.
+           """
+        def _AccountFix():
+            if function_name == 'AddAccount':
+                new_res.update({'AccountSearch': y})
+                # this allows AddAccount to populate its AccountNumber
+                # field without using the entire __str__ value
+                new_res.update({'AccountNumber': y.split(' - ')[1]})
+
+            else:
+                new_res.update({'AccountSearch': y})
+            logger.debug("new res was updated")
+            try:
+                if function_name != 'AddAccount':
+                    new_res.pop('AccountNumber')
+            except KeyError as e:
+                pass
+
+        row_id = int(list_of_results[0]['row_id'])
+
+        try:
+            if not function_name:
+                # this is the name of the function that called PrePopulate
+                f_name = istack()[2][3]
+                function_name = f_name
+        except KeyError as e:
+            logger.warning("falling back to passed in function name...")
+            if not function_name:
+                raise AttributeError("no function_name attribute to fall back on.")
+        except TypeError as e:
+            logger.warning("falling back to passed in function name...")
+            if not function_name:
+                raise AttributeError("no function_name attribute to fall back on.")
+        except Exception as e:
+            logger.error(e)
+            logger.warning("falling back to passed in function name...")
+            if not function_name:
+                raise AttributeError("no function_name attribute to fall back on.")
+
+        if list_of_results and row_id:
+            for res in list(list_of_results):
+                for k, v in res.items():
+                    if k == 'id' and int(v) == int(row_id):
+                        # print(k, v, row_id)
+                        # res.keys() are the model field names.
+                        """ Since the intellitext fields (for the most part) are ModelFieldName + 'Search',
+                        creating a dict with the model field names as keys and the intellitext fields
+                        as values will work to re-map the results for editing.
+                        Anything that isn't an intellitext field OR is already formatted with Search
+                        on the end is appended with a second list comp. """
+                        SearchKeys = ([{x: str(x + 'Search')} for x in res.keys()
+                                       if x != 'id' and not x.endswith('Search') and x not in cls().fields]
+                                      + [{x: x} for x in res.keys() if
+                                         x.endswith('Search') or x == 'id' or x in cls().fields])
+                        # since SearchKeys is always going to be a list with a single entry,
+                        # use ChainMap to turn it into one unified dict
+                        SearchKeys = dict(ChainMap(*SearchKeys))
+
+                        new_res = {}
+                        # go back through the whole results list entry
+                        for x, y in res.items():
+                            if x in SearchKeys:
+                                new_res.update({SearchKeys[x]: y})
+                                logger.debug("new res was updated")
+                            if x == 'AccountNumber':
+                                _AccountFix()
+
+                        # print(new_res.items())
+                        # cls().fields are the Intellitext search fields
+                        return cls(initial=new_res)
+            raise ValidationError("no row match found, could not pre-populate")
+
+        else:
+            raise AttributeError("initial_values and row_id must be provided in order to pre-populate the form")
+
+
+class IntellitextBaseForm(PrePopulatedForm):
     """For use with non ModelForm form classes and acts as a base class for IntellitextModelForm."""
 
     def __init__(self, *args, **kwargs):
