c59129ad89879aac1607e081535fb40a2d62b2f4
Andrew McSparron <Amcsparron@albanyny.gov>
2024-02-14 13:50:28 -0500

    updated setup.py version attr to 1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='DjangoIntellitextFormField',
-    version='1.0',
+    version='1.5',
     packages=['DjangoIntellitextFormField'],
     url='https://github.com/amcsparron2793-Water/DjangoIntellitextFormField',
     download_url='https://github.com/amcsparron2793-Water/DjangoIntellitextFormField/archive/refs/tags/1.5.tar.gz',
